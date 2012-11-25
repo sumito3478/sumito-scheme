2262245d59c24248ed937ca3c9b224de47b46a06
sumito3478@gmail.com <sumito3478@gmail.com>
2012-11-25 14:01:20 +0900

    Add spec for SchemeChar implicit conversion

--- src/test/scala/sumito3478/scheme/SchemeCharSpec.scala
+++ src/test/scala/sumito3478/scheme/SchemeCharSpec.scala
@@ -4,4 +4,15 @@ import org.specs2.mutable.Specification
 
 class SchemeCharSpec  extends Specification{
 
+  "SchemeChar" should {
+    "implicitly converted from Int" in {
+      val schemeChar : SchemeChar = 0x20
+      schemeChar.value mustEqual 0x20
+    }
+    "implicitly converted to Int" in {
+      val schemeChar : SchemeChar = 0x20
+      val int : Int = schemeChar
+      int mustEqual 0x20
+    }
+  }
 }
