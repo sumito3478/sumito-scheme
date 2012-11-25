0950cddb83bf60ed8966d44aaeac35aaef699acf
sumito3478@gmail.com <sumito3478@gmail.com>
2012-11-25 18:46:55 +0900

    Add spec for SchemeString#toString

--- src/test/scala/sumito3478/scheme/SchemeStringSpec.scala
+++ src/test/scala/sumito3478/scheme/SchemeStringSpec.scala
@@ -3,6 +3,16 @@ package sumito3478.scheme
 import org.specs2.mutable.Specification
 
 class SchemeStringSpec extends Specification{
+  "SchemeString#toString" should {
+    trait context extends Scope{
+      val orig : String = "Unicode Character 'PALM TREE' (U+1F334) : '\ud83c\udf34' (UTF-16: 0xd83c 0xdf34)"
+      val s : SchemeString = orig
+    }
+
+    "return String equals to the original" in new context {
+      s.toString() mustEqual orig
+    }
+  }
   "SchemeString#toCharArray" should {
     trait context extends Scope {
       val buffer = Array[SchemeChar](0x1f334, 0x1f333)
