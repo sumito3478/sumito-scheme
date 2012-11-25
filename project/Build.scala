d5ce90cdfc6b2ce51f86a0db2a6bcecd2a2936d8
sumito3478@gmail.com <sumito3478@gmail.com>
2012-11-25 13:09:33 +0900

    Add basic project settings to Build.scala

---
+++ project/Build.scala
@@ -0,0 +1,19 @@
+import sbt._
+import Keys._
+import com.twitter.sbt._
+
+object SumitoSchemeBuild extends Build {
+
+  lazy val sumitoScheme = Project(
+    id = "sumito-scheme", 
+    base = file("."), 
+    settings = StandardProject.newSettings
+  ).settings(
+    Seq(
+      version := "0.0.1", 
+      scalaVersion := "2.10.0-RC2"
+    ): _*
+  )
+
+}
+
