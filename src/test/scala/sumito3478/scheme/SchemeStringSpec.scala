60878006874d3db1f178ef9424c7218869fa7577
sumito3478@gmail.com <sumito3478@gmail.com>
2012-11-25 18:41:48 +0900

    Add spec for SchemeString#toCharArray

--- src/test/scala/sumito3478/scheme/SchemeStringSpec.scala
+++ src/test/scala/sumito3478/scheme/SchemeStringSpec.scala
@@ -3,4 +3,15 @@ package sumito3478.scheme
 import org.specs2.mutable.Specification
 
 class SchemeStringSpec extends Specification{
+  "SchemeString#toCharArray" should {
+    trait context extends Scope {
+      val buffer = Array[SchemeChar](0x1f334, 0x1f333)
+      val s : SchemeString = "\ud83c\udf34\ud83c\udf33" // same as above buffer
+    }
+
+    "return Array of SchemeChar contained in the SchemeString" in new context{
+      s.toCharArray mustEqual buffer
+    }
+  }
 }
+
