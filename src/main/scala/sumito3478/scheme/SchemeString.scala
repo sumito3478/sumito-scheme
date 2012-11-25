5f026673862d6d1ed4a9f26c23e76f0684450dcc
sumito3478@gmail.com <sumito3478@gmail.com>
2012-11-25 14:23:47 +0900

    Add SchemeString object

--- src/main/scala/sumito3478/scheme/SchemeString.scala
+++ src/main/scala/sumito3478/scheme/SchemeString.scala
@@ -3,5 +3,10 @@ package sumito3478.scheme
 import scala.collection.mutable.ArrayBuffer
 
 class SchemeString(private val buffer : ArrayBuffer[SchemeChar] = new ArrayBuffer[SchemeChar]) {
+}
+
+object SchemeString{
 
 }
+
+
