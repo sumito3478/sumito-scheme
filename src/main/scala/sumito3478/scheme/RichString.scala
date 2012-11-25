fcf11153eda40770c1b8d991b7c18e0513116ee9
sumito3478@gmail.com <sumito3478@gmail.com>
2012-11-25 18:53:18 +0900

    Add RichString#toSchemeCharArrayBuffer

--- src/main/scala/sumito3478/scheme/RichString.scala
+++ src/main/scala/sumito3478/scheme/RichString.scala
@@ -1,6 +1,21 @@
 package sumito3478.scheme
 
+import collection.mutable.ArrayBuffer
+
 class RichString(val value : String) {
+  def toSchemeCharArrayBuffer : ArrayBuffer[SchemeChar] = {
+    val count = value.codePointCount(0, value.length)
+    val buffer = new ArrayBuffer[SchemeChar](count)
+    var cpIndex = 0
+    var charIndex = 0
+    while(cpIndex < count) {
+      val cp = value.codePointAt(charIndex)
+      buffer += cp
+      charIndex += Character.charCount(cp)
+      cpIndex += 1
+    }
+    buffer
+  }
 
   override def toString() = value
 }
