ea31c90c39c3606cc56de72690d93462f8561ee5
sumito3478 <sumito3478@gmail.com>
2012-11-27 23:27:35 +0900

    Modify specs to extends SpecificationWithJUnit

--- src/test/scala/sumito3478/scheme/SchemeCharSpec.scala
+++ src/test/scala/sumito3478/scheme/SchemeCharSpec.scala
@@ -1,12 +1,9 @@
 package sumito3478.scheme
 
-import org.specs2.mutable.Specification
+import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.specification.Scope
-import org.junit.runner.RunWith
-import org.specs2.runner.JUnitRunner
 
-@RunWith(classOf[JUnitRunner])
-class SchemeCharSpec  extends Specification{
+class SchemeCharSpec  extends SpecificationWithJUnit{
   trait context extends Scope {
     val v = 0x1f334 // PALM TREE
   }
