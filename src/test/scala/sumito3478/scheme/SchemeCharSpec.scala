553a7263b0dfd4ec5b078d2a0e714af042c7b9b2
sumito3478 <sumito3478@gmail.com>
2012-11-29 02:44:36 +0900

    Format SchemeCharSpec.scala with eclipse

--- src/test/scala/sumito3478/scheme/SchemeCharSpec.scala
+++ src/test/scala/sumito3478/scheme/SchemeCharSpec.scala
@@ -3,7 +3,7 @@ package sumito3478.scheme
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.specification.Scope
 
-class SchemeCharSpec  extends SpecificationWithJUnit{
+class SchemeCharSpec extends SpecificationWithJUnit {
   trait context extends Scope {
     val v = 0x1f334 // PALM TREE
   }
